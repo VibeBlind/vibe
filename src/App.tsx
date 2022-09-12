e971bda4fca7436765b111f45143aa23291c3c10
Seth <sethg13@gmail.com>
2022-09-12 13:35:30 -0500

    Create point grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import * as THREE from 'three';
+import {Canvas, useLoader} from '@react-three/fiber';
+import React, { Suspense, useCallback, useMemo, useRef } from 'react';
+import circleImg from "./assets/circle.png"
+//const circleImg = require("./assets/circle.png")
 
-function App() {
+function Points() {
+  const imgTex = useLoader(THREE.TextureLoader, circleImg) as THREE.Texture;
+  
+  const count = 100
+  const sep = 3
+  let positions = useMemo(() => {
+    let positions = []
+
+    for(let xi = 0; xi < count; xi++) {
+      for(let zi = 0; zi < count; zi++) {
+        let x = sep * (xi - count / 2);
+        let z = sep * (zi - count / 2);
+        let y = 0
+        positions.push(x, y, z);
+      }
+    }
+
+    return new Float32Array(positions);
+  }, [count, sep])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <points>
+      <bufferGeometry attach="geometry">
+        <bufferAttribute
+          attach="attributes-position"
+          array={positions}
+          count={positions.length / 3}
+          itemSize={3}
+        />
+      </bufferGeometry>
+
+      <pointsMaterial
+        attach="material"
+        map={imgTex}
+        color={0x00AAFF}
+        size={0.5}
+        sizeAttenuation
+        transparent={false}
+        alphaTest={0.5}
+        opacity={1.0}
+      />
+    </points>
+  )
+}
+
+function AnimationCanvas() {
+  return (
+    <Canvas
+      camera={{position:[100, 10, 0], fov: 75}}
+    >
+      <Suspense fallback={null}>
+        <Points/>
+      </Suspense>
+
+    </Canvas>
+  )
+}
+
+const App = () => {
+  return (
+    <div className="anim">
+      <AnimationCanvas/>
     </div>
   );
-}
+};
 
 export default App;
