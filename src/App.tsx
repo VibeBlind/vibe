93df6e685a7eb1af7d76cd28d65e7cda97be544a
Seth <sethg13@gmail.com>
2022-09-13 20:03:30 -0500

    Correct wave function

--- src/App.tsx
+++ src/App.tsx
@@ -4,41 +4,39 @@ import { OrbitControls } from '@react-three/drei';
 import { Canvas, useFrame, useLoader } from '@react-three/fiber';
 import { Suspense, useCallback, useMemo, useRef } from 'react';
 import circleImg from "./assets/circle.png"
-//const circleImg = require("./assets/circle.png")
 
 function Points() {
   const imgTex = useLoader(THREE.TextureLoader, circleImg) as THREE.Texture;
   //const bufferRef = useRef();
 
-  // Wave Function
-  let shift = 0; //phase shift
-  let freq = 0.00;
-  let amp = 3;
-  const graph = useCallback((x: number, z: number) => {
-    return Math.sin(freq * (x**2 + z**2 + shift)) * amp;
-  }, [shift, freq, amp])
+  let t = 0;
+  let f = 0.002;
+  let a = 3;
+  const graph = useCallback((x: number, z:number) => {
+    return Math.sin(f * (x ** 2 + z ** 2 + t)) * a;
+  }, [t, f, a])
 
-  // Place points on graph
   const count = 100
   const sep = 3
   let positions = useMemo(() => {
     let positions = []
 
-    for(let xi = 0; xi < count; xi++) {
-      for(let zi = 0; zi < count; zi++) {
+    for (let xi = 0; xi < count; xi++) {
+      for (let zi = 0; zi < count; zi++) {
         let x = sep * (xi - count / 2);
         let z = sep * (zi - count / 2);
-        let y = graph(x, z)
+        let y = graph(x, z);
         positions.push(x, y, z);
       }
     }
 
     return new Float32Array(positions);
-  }, [count, sep])
-
+  }, [count, sep, graph])
+  /**
   useFrame(() => {
-    shift += 15
-    //const positions = bufferRef.current.array;
+    t += 15
+    
+    const positions = bufferRef.current.array;
 
     let i = 0;
     for (let xi = 0; xi < count; xi++) {
@@ -51,16 +49,16 @@ function Points() {
       }
     }
 
-    //bufferRef.current.needsUpdate = true;
+    bufferRef.current.needsUpdate = true;
   })
+  */
 
-  // Put point graph in buffer
   return (
     <points>
       <bufferGeometry attach="geometry">
         <bufferAttribute
-          //ref = {bufferRef}
-          attach="attributes-position"
+          //ref={bufferRef}
+          attach ="attributes-position"
           array={positions}
           count={positions.length / 3}
           itemSize={3}
@@ -78,7 +76,7 @@ function Points() {
         opacity={1.0}
       />
     </points>
-  )
+  );
 }
 
 function AnimationCanvas() {
@@ -94,9 +92,11 @@ function AnimationCanvas() {
 
 const App = () => {
   return (
-    <div className="anim">
-      <AnimationCanvas/>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="anim">
+        <AnimationCanvas/>
+      </div>
+    </Suspense>
   );
 };
 
